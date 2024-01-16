347e57990622bd356925b8cd7f4002ca180133cc
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 16:22:18 +0200

    Delete Programming basics/programing Basics 2 directory

--- Programming basics/programing Basics 2/Supplies for School.py
+++
@@ -1,8 +0,0 @@
-number_of_pen_packs = int(input())
-number_of_marker_packs = int(input())
-litres_of_detergent = int(input())
-percent_discount = int(input())
-
-total_price = number_of_pen_packs * 5.80 + number_of_marker_packs * 7.20 + litres_of_detergent * 1.20
-price_with_discount = total_price - (total_price * percent_discount / 100)
-print(price_with_discount)
--- Programming basics/programing Basics 2/oscars_ceremony.py
+++
@@ -1,9 +0,0 @@
-rent = int(input())
-
-statues_price = rent * 0.7
-catering_price = statues_price * 0.85
-sound_price = catering_price / 2
-
-total_price = rent + statues_price + catering_price + sound_price
-
-print(f'{total_price:.2f}')

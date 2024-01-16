5a2d3eb29eba1124ada36539425459eec2f398f6
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 17:31:36 +0200

    Delete Programming Basics with Python - november 2023 directory

--- Programming Basics with Python - november 2023/Tasks 2/Projects Creation.py
+++
@@ -1,6 +0,0 @@
-name_of_architect = input()
-number_of_projects = int(input())
-
-time_for_projects = number_of_projects * 3
-
-print(f"The architect {name_of_architect} will need {time_for_projects} hours to complete {number_of_projects} project/s.")
--- Programming Basics with Python - november 2023/Tasks 2/gifts_from_santa.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-m = int(input())
-s = int(input())
-
-for adress in range(m, n - 1, -1):
-    if adress % 2 == 0 and adress % 3 == 0:
-        if adress == s:
-            break
-        print(f"{adress}", end=" ")

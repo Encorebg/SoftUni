d722ad931f1a40747415f2820fedeb53b64324c0
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 16:34:20 +0200

    Delete SoftUni/Tasks 1 directory

--- SoftUni/Tasks 1/combinations.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-combination_counter = 0
-for x in range(26):
-    for y in range(26):
-        for z in range(26):
-            if x + y + z == n:
-                combination_counter += 1
-
-print(combination_counter)

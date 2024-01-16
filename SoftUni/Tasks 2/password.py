6923cb2d30bd47be795e90f564b19cea8accfe30
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 16:34:29 +0200

    Delete SoftUni/Tasks 2 directory

--- SoftUni/Tasks 2/password.py
+++
@@ -1,9 +0,0 @@
-username = input()
-password = input()
-
-data = input()
-
-while password != data:
-    data = input()
-
-print(f'Welcome {username}!')
--- SoftUni/Tasks 2/sequence_2k_1.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-
-sequence = 1
-
-while sequence <= n:
-    print(sequence)
-    sequence = 2 * sequence + 1

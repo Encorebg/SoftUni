8106b51d40e64c8bc4019a53f9cc89c06ff09928
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 16:21:21 +0200

    Delete Programming basics/programming basics 1 directory

--- Programming basics/programming basics 1/Yard Greening.py
+++
@@ -1,8 +0,0 @@
-square_metres_for_greening = float(input())
-
-total_price = square_metres_for_greening * 7.61
-discount = total_price * 0.18
-price_with_discount = total_price - discount
-
-print(f"The final price is: {price_with_discount} lv.")
-print(f"The discount is: {discount} lv.")

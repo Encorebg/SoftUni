a5d806301e1740845b57dc4bfeeea0430a44c1dc
Encorebg <103518756+Encorebg@users.noreply.github.com>
2024-01-16 16:45:53 +0200

    Delete Programming Basics directory

--- Programming Basics/Tasks 1/Deposit Calculator.py
+++
@@ -1,8 +0,0 @@
-deposited_sum = float(input())
-depoosit_term = int(input())
-annual_interest_rate = float(input())
-
-generated_rate_per_year = deposited_sum * annual_interest_rate / 100
-generated_rate_per_month = generated_rate_per_year / 12
-total_sum = deposited_sum + depoosit_term * generated_rate_per_month
-print(total_sum)

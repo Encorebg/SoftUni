c86f52369301d0e8c69085e847e3f734b2b709e6
Nikola Velikov <103518756+Encorebg@users.noreply.github.com>
2024-01-21 19:08:28 +0200

    Add files via upload

--- Programming Basics with Python - November 2023/Tasks 2/test.js
+++
@@ -1,9 +0,0 @@
-for (let i = 1 ; i < 3; i++){
-    for(let j = 1; j < 3; j++){
-        for(let z = 1; z < 3; z++){
-            for(let k = 1; k < 3; k++ ){
-                console.log(k)
-            }
-        }
-    }
-}

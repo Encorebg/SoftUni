96ee6e2852e9dbeffd902d461489a06af2ba39cb
Nikola Velikov <103518756+Encorebg@users.noreply.github.com>
2024-02-22 17:48:56 +0200

    Add files via upload

---
+++ Programming Fundamentals with JavaScript - January 2024/6. Objects and Classes - Tasks/convertToJSON.js
@@ -0,0 +1,4 @@
+function convertToJSON(name, lastName, hairColor){
+    let res = {name, lastName, hairColor};
+    console.log(JSON.stringify(res));
+}
---
+++ Programming Fundamentals with JavaScript - January 2024/6. Objects and Classes - Tasks/personInfo.js
@@ -0,0 +1,8 @@
+function personInfo(firstName, lastName, age){
+    let resullt = {
+        firstName : firstName,
+        lastName : lastName,
+        age : age
+    };
+    return resullt;
+}
